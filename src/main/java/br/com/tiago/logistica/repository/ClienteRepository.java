dd8d7c022f3a4f0e8c0456bccbbd0d7f95bc49f5
Tiago <tiagobsb84@gmail.com>
2022-09-20 13:45:59 -0300

    Usando Data JPA

---
+++ src/main/java/br/com/tiago/logistica/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package br.com.tiago.logistica.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.tiago.logistica.models.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+}
